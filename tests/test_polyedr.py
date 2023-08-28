e5abe7794648da48330f896b19f37512126abeda
c0yver <143102301+c0yver@users.noreply.github.com>
2023-08-28 21:15:59 +0300

    Add files via upload

--- tests/test_polyedr.py
+++ tests/test_polyedr.py
@@ -5,9 +5,9 @@ i = 0
 a = [0]*6
 a[0] = 25.0
 for j in range(5):
-	a[j+1] = 0
+    a[j+1] = 0
 
 for name in ["ccc", "cube", "box", "king", "cow", "babem"]:
-	polyedr = Polyedr(f"data/{name}.geom")
-	assert polyedr.sum_area() == approx(a[i])
-	i += 1
+    polyedr = Polyedr(f"data/{name}.geom")
+    assert polyedr.sum_area() == approx(a[i])
+    i += 1
